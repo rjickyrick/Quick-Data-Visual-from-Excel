37644ef3398dedb38f9bfa6ef13c500774ecfbb7
rjickyrick <106218721+rjickyrick@users.noreply.github.com>
2022-08-18 13:13:42 +0700

    Update Specialty Surgeries to Date.py

--- Specialty Surgeries to Date.py
+++ Specialty Surgeries to Date.py
@@ -3,7 +3,7 @@ import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-data = pd.read_csv("Specialty Surgeries to Date.xlsx")
+data = pd.read_csv("Specialty Surgeries to Date.csv")
 print(data)
 print(data.columns)
 sns.relplot(x="entity",y="value",data=data)
